a71fed5526cd4b91de8eb17c8ef9787777062620
Dongbumlee <donlee@microsoft.com>
2018-01-22 13:39:48 +0900

    setup node script

---
+++ setupenv.sh
@@ -0,0 +1,53 @@
+##Eninstall Go old version
+sudo apt-get remove golang-go
+sudo apt-get remove --auto-remove golang-go
+##Clearning Go Env
+rm * -r -f
+sudo rm /usr/local/go -r -f
+##Update Sysetm
+sudo apt-get update
+sudo apt-get -y upgrade
+##Download Go
+wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
+##Extract
+sudo tar -xvf go1.9.2.linux-amd64.tar.gz
+##Move go
+sudo mv go /usr/local
+##Install Quorum
+git clone https://github.com/jpmorganchase/quorum.git
+
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/quorum
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+##Compile
+cd quorum
+make all
+cd ..
+export PATH=$PATH:/home/gethadmin/quorum/build/bin
+##Download Constellation
+sudo apt-get update
+sudo apt-get -y upgrade
+
+echo Y | sudo apt-get install libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev
+curl -sSL https://get.haskellstack.org/ | sh
+
+export PATH=/home/gethadmin/.local/bin:$PATH
+
+stack setup
+git clone https://github.com/jpmorganchase/constellation.git
+cd constellation
+stack install
+
+cd ..
+
+# install nodejs 6.x
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=$PATH:/home/gethadmin/.local/bin
+
+go get github.com/getamis/istanbul-tools/cmd/istanbul
