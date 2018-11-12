c635043233b2e1d23d71e55e635162ef03658b9d
DB Lee <donlee@microsoft.com>
2018-11-12 14:44:50 +0900

    update related path

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 ecoh "stop processes"
 bash ./stop.sh
 echo "clear datas"
-rm .ethereum/geth/chaindata -r -f
-rm .ethereum/geth/lightchaindata -r -f
+rm ~/.ethereum/geth/chaindata -r -f
+rm ~/.ethereum/geth/lightchaindata -r -f
 echo "initialize genesis block"
-geth init genesis.json
+geth init ~/genesis.json
 echo clear logfile
-rm gethlog.log
-rm constellation/constellation.log
+rm ~/gethlog.log
+rm ~/constellation/constellation.log
